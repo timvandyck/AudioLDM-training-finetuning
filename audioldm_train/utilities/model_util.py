64b88516033348bc40f0670cbddcc9069572a27e
timvandyck <tim.van.dyck13@gmail.com>
2025-07-24 11:30:30 +0200

    added log of currently loading module

--- audioldm_train/utilities/model_util.py
+++ audioldm_train/utilities/model_util.py
@@ -104,6 +104,7 @@ def instantiate_from_config(config):
 
 def get_obj_from_str(string, reload=False):
     module, cls = string.rsplit(".", 1)
+    print(f"currently loading {module}.{cls}...")
     if reload:
         module_imp = importlib.import_module(module)
         importlib.reload(module_imp)
